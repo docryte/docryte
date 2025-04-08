1d415ffe93f17fe01808a9b871cfad8674b68491
docryte <docryte@gmail.com>
2025-04-08 13:50:40 +0300

    Simplify code structure

--- main.go
+++ main.go
@@ -16,23 +16,21 @@ func handleStatic(mux *http.ServeMux) {
 func main() {
 	mux := http.NewServeMux()
 	handleStatic(mux)
-	temp, err := template.ParseFiles("./templates/main.html")
-	if err != nil {
+
+	if temp, err := template.ParseFiles("./templates/main.html"); err != nil {
 		log.Fatal("Error while loading template: ", err)
+	} else {
+		mux.HandleFunc("/", indexPage(temp))
 	}
 
-	mux.HandleFunc("/", indexPage(temp))
-
-	err = http.Serve(autocert.NewListener("docryte.site"), mux)
-	if err != nil {
+	if err := http.Serve(autocert.NewListener("docryte.site"), mux); err != nil {
 		log.Fatal("Error while starting server: ", err)
 	}
 }
 
 func indexPage(template *template.Template) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
-		err := template.Execute(w, nil)
-		if err != nil {
+		if err := template.Execute(w, nil); err != nil {
 			w.WriteHeader(500)
 			w.Write([]byte(err.Error()))
 		}
